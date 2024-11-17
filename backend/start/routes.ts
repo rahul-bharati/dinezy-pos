baea5693ed0b8eee141e2ea6b8181691cb8932dc
Rahul Bharati <rahulrbharati@gmail.com>
2024-11-18 01:40:07 +0530

    add 404 not available api

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -10,3 +10,12 @@
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => 'It works!')
+router
+  .group(() => {
+    router.get('/', async () => ({
+      Hello: 'World',
+    }))
+  })
+  .prefix('/api')
+
+router.any('*', ({ response }) => response.status(404).send({ message: 'Not Found' }))
